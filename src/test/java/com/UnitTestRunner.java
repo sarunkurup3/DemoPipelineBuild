f9bc0d3ddfe07d74fe3f3348b30dce082cc51277
sarunkurup <sarunkurup3@gmail.com>
2021-06-16 00:22:42 +0530

    Added test run method

--- src/test/java/com/UnitTestRunner.java
+++ src/test/java/com/UnitTestRunner.java
@@ -1,5 +1,23 @@
 package com;
 
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
 public class UnitTestRunner {
 
+	@BeforeTest
+	public void beforeTest() {
+		System.out.println("Before test");
+	}
+
+	@Test
+	public void testMethod() {
+		System.out.println("Test Method");
+	}
+	
+	@AfterTest
+	public void afterTest() {
+		System.out.println("After Test");
+	}
 }
